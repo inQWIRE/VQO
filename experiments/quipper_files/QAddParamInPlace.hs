7b0b6d4ced700ff54a7fe7e959cb1aa00986cbb7
Finn Voichick <finn@umd.edu>
2022-04-15 20:20:08 -0500

    Updated OpenQASM files and generator script

--- experiments/quipper_files/QAddParamInPlace.hs
+++ experiments/quipper_files/QAddParamInPlace.hs
@@ -1,5 +1,9 @@
+import System.Environment
 import Quipper
 import Quipper.Libraries.Arith
 
 main :: IO ()
-main = print_generic ASCII (q_add_param_in_place (intm 16 38168)) (qshape (intm 16 0))
+main = do
+  [x] <- getArgs
+  let c = read x
+  print_generic ASCII (q_add_param_in_place (intm 16 c)) (qshape (intm 16 0))
--- experiments/quipper_files/QMultParam.hs
+++ experiments/quipper_files/QMultParam.hs
@@ -1,5 +1,9 @@
+import System.Environment
 import Quipper
 import Quipper.Libraries.Arith
 
 main :: IO ()
-main = print_generic ASCII (q_mult_param (intm 16 38168)) (qshape (intm 16 0))
+main = do
+  [x] <- getArgs
+  let c = read x
+  print_generic ASCII (q_mult_param (intm 16 c)) (qshape (intm 16 0))
